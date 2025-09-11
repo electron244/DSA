612b7c6673527736d6524a54bc08f0517e89a611
Sidharth Sinh <zalasidharth244@gmail.com>
2025-09-11 14:30:50 +0530

    Random Question 01

---
+++ Questions/Palindrone_Number_09.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false; // Negative numbers are not palindrome
+    }
+
+    int original = x;
+    long long reverse = 0;
+
+    while (x > 0) {
+        int digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = x / 10;
+    }
+
+    return original == reverse;
+}
+
+int main() {
+    int num = 121;
+
+    if (isPalindrome(num)) {
+        cout << num << " is a palindrome." << endl;
+    } else {
+        cout << num << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
