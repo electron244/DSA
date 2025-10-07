b95dc06a61cbb8b5c771bde5dfa84d0feb90b3dc
Sidharth Sinh <zalasidharth244@gmail.com>
2025-10-07 21:33:07 +0530

    Leet Code 1700

---
+++ Queue/LeetCode_1700.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    queue<int> q;
+    for (int s : students) q.push(s);
+    
+    int i = 0;  // index for sandwiches
+    int count = 0;
+    
+    while (!q.empty() && count < q.size()) {
+        if (q.front() == sandwiches[i]) {
+            q.pop();
+            i++;
+            count = 0;
+        } else {
+            q.push(q.front());
+            q.pop();
+            count++;
+        }
+    }
+    
+    return q.size();
+}
+
+int main() {
+    vector<int> students = {1,1,0,0};
+    vector<int> sandwiches = {0,1,0,1};
+    cout << countStudents(students, sandwiches);
+    return 0;
+}
